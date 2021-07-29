243f47a827e640694eb827656e4748b2193fde3e
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2021-07-30 08:48:46 +1200

    Create users table & active record model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: 'Han Solo', username: 'han.solo@example.com')
+User.create!(name: 'Luke Skywalker', username: 'luke.skywalker@example.com')
+User.create!(name: 'Rey Skywalker', username: 'rey.skywalker@example.com')
+User.create!(name: 'Poe Dameron', username: 'poe.dameron@example.com')
+User.create!(name: 'Leia Organa', username: 'leia.organa@example.com')
